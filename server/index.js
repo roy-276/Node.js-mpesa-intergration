f2fc1238593458508d354d2491d52b06b61c7c96
roy-276 <maingiroy95@gmail.com>
2022-10-26 02:12:50 +0300

    complete app

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 
 const app = express();
+require('dotenv').config();
+const cors = require('cors');
+const TokenRoute = require('./routes/token');
 
 app.listen(5000, () => {
   console.log('Server Running');
 });
 
+app.use(express.json());
+app.use(cors());
 app.get('/', (req, res) => {
   res.send('Mpesa');
 });
+
+app.use('/token', TokenRoute);
